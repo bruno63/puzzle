b8c2389a399ca3b35872ea0603e640b3808e2a1d
Bruno Kaiser <bruno@bkaiser.ch>
2015-01-17 16:50:43 +0100

    git submodule re-setup overwrote whole app with an old version; had to re-implement the whole app

--- puzzle.controller.js
+++ puzzle.controller.js
@@ -1,9 +1,10 @@
 'use strict';
 
 angular.module('puzzle')
-.controller('puzzleCtrl', function($scope, $translatePartialLoader, AppConfig) {
-	AppConfig.setCurrentApp('Puzzle', 'fa-puzzle-piece', 'puzzle', '');
+.controller('puzzleCtrl', function($scope, $translatePartialLoader, $log, cfg) {
+	cfg.GENERAL.CURRENT_APP = 'puzzle';
 	$translatePartialLoader.addPart('puzzle');
+	$log.log('puzzleCtrl/cfg = ' + JSON.stringify(cfg, null, '\t'));
 
  	$scope.puzzles = [
 	 	{ src: 'app/puzzle/img/cat.jpg',         title: 'Cat',       rows: 4, cols: 4 },
@@ -14,4 +15,18 @@ angular.module('puzzle')
 	 	{ src: 'app/puzzle/img/sheep.jpg',       title: 'Sheep',     rows: 4, cols: 4 },
 	 	{ src: 'app/puzzle/img/zebra.jpg',       title: 'Zebra',     rows: 4, cols: 4 },
  	];
+	$scope.puzzle = $scope.puzzles[2];
+
+	$scope.showInfo = function() {
+		$log.log('puzzleCtrl.showInfo() = ' + cfg.puzzle.SHOW_INFO);
+		return cfg.puzzle.SHOW_INFO;
+	};
+
+	$scope.solve = function() {
+		$scope.puzzle.api.solve();
+	};
+
+	$scope.shuffle = function() {
+		$scope.puzzle.api.shuffle();
+	};
 });
